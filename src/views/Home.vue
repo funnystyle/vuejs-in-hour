62e31577c25e91508fe0c4a1da22b0b1c72f13ba
Funnystyle <esero@hanmail.net>
2021-04-24 14:07:13 +0900

    data binding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <h1>Welcome to Home!</h1>
+    <h1>Welcome to {{ title2 }}!</h1>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: "개발자의 품격",
+      title2: "Seoul",
+    };
+  },
+};
 </script>
